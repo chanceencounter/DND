d63b5eae7bcba2e6e1151c25bbf88689845b9282
Erica Rao <ericarao.x@gmail.com>
2016-11-21 18:17:57 -0500

    Updating error in login credential objects.

--- DNDCore/src/main/java/com/ericarao/dnd/core/model/AbstractDMLoginCredentials.java
+++ DNDCore/src/main/java/com/ericarao/dnd/core/model/AbstractDMLoginCredentials.java
@@ -5,10 +5,10 @@ import org.immutables.value.Value;
 @DndStyle
 @Value.Immutable
 public abstract class AbstractDMLoginCredentials implements NetworkPacket{
-    public abstract String roomName;
-    public abstract int numPlayers;
-    public abstract String roomPassword;
-    public abstract String dmIP;
+    public abstract String roomName();
+    public abstract int numPlayers();
+    public abstract String roomPassword();
+    public abstract String dmIP();
 
     @Override
     @Value.Derived
--- DNDCore/src/main/java/com/ericarao/dnd/core/model/AbstractPlayerLoginCredentials.java
+++ DNDCore/src/main/java/com/ericarao/dnd/core/model/AbstractPlayerLoginCredentials.java
@@ -5,10 +5,12 @@ import org.immutables.value.Value;
 @DndStyle
 @Value.Immutable
 public abstract class AbstractPlayerLoginCredentials implements NetworkPacket{
-    public abstract String roomName;
-    public abstract int numPlayers;
-    public abstract String roomPassword;
-    public abstract String dmIP;
+    public abstract String roomName();
+    public abstract String dmIP();
+    public abstract String playerName();
+    public abstract String roomPassword();
+    public abstract String playerIP();
+
 
     @Override
     @Value.Derived
