15b9aabac8ce1dce52aad2361cabf81e97b51bd3
Erica Rao <ericarao.x@gmail.com>
2016-11-22 11:26:44 -0500

    Update to InputValidation, needs testing/debugging.

--- DNDCore/src/main/java/com/ericarao/dnd/core/View/InputValidation.java
+++ DNDCore/src/main/java/com/ericarao/dnd/core/View/InputValidation.java
@@ -1,9 +1,34 @@
 package com.ericarao.dnd.core.View;
 
-import java.awt.event.KeyEvent;
+import javafx.scene.control.TextField;
 
 public class InputValidation {
-    public void CharacterValidation(KeyEvent keyEvent) {
-        //String input = keyEvent;
+
+    //Need to include threads for listening to keystrokes.
+    private final String regex = "\\d+";
+
+    //Number or Digit Input Validation
+    public void numberValidation(TextField textField) {
+
+        boolean isPressed = false;
+        //Numbers
+        while (textField.getOnKeyPressed().equals(isPressed)){
+            if (textField.getOnKeyPressed().equals(regex)) {
+                textField.setEditable(true);
+            }
+            else {
+                textField.setEditable(false);
+            }
+        }
+
     }
+
+    public String valid (TextField textField) {
+        if (!textField.getOnKeyPressed().equals(regex)){
+            return "";
+        }
+        else return "Invalid input.";
+    }
+
+
 }
