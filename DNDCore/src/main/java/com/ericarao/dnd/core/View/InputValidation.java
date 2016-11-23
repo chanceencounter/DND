8a9fb775bed5f2257d974207e4fbb408400c99fa
Erica Rao <ericarao.x@gmail.com>
2016-11-22 22:10:51 -0500

    Functioning input validator for playernum

--- DNDCore/src/main/java/com/ericarao/dnd/core/View/InputValidation.java
+++ DNDCore/src/main/java/com/ericarao/dnd/core/View/InputValidation.java
@@ -1,34 +1,50 @@
 package com.ericarao.dnd.core.View;
 
+import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class InputValidation {
 
     //Need to include threads for listening to keystrokes.
-    private final String regex = "\\d+";
+    private final String regex = "[1-9]";
+    //private boolean isPressed = false;
 
     //Number or Digit Input Validation
     public void numberValidation(TextField textField) {
 
-        boolean isPressed = false;
-        //Numbers
-        while (textField.getOnKeyPressed().equals(isPressed)){
-            if (textField.getOnKeyPressed().equals(regex)) {
+        textField.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                //isPressed = true;
+                //Error: For some reason the key pressed is capturing "null"
+                if (textField.getText().matches(regex) && Integer.parseInt(textField.getText()) < 9) {
+                    //isPressed = false;
+                } else {
+                    textField.setEditable(false);
+                    textField.setText("");
+                    textField.setEditable(true);
+
+                }
+            }
+        });
+
+        /*textField.addEventHandler(KeyEvent.KEY_RELEASED, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                isPressed = false;
                 textField.setEditable(true);
             }
-            else {
-                textField.setEditable(false);
-            }
-        }
+        });*/
 
     }
 
-    public String valid (TextField textField) {
-        if (!textField.getOnKeyPressed().equals(regex)){
+
+    /*
+        public String valid(TextField textField) {
+        if (!textField.getOnKeyPressed().equals(regex)) {
             return "";
-        }
-        else return "Invalid input.";
+        } else return "Invalid input.";
     }
-
-
+    */
 }
