03b55f35bcd929f560504433a7d4f94cf2c8c8cc
Erica Rao <ericarao.x@gmail.com>
2016-11-27 20:08:28 -0500

    Updating PlayerGridPane functionality

--- DNDCore/src/main/java/com/ericarao/dnd/core/View/PlayerGridPane.java
+++ DNDCore/src/main/java/com/ericarao/dnd/core/View/PlayerGridPane.java
@@ -2,6 +2,7 @@ package com.ericarao.dnd.core.View;
 
 import com.ericarao.dnd.core.model.ClientUpdate;
 import com.ericarao.dnd.core.model.RegisterPlayer;
+import com.ericarao.dnd.core.model.UpdatePlayerStats;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -14,79 +15,95 @@ import javafx.scene.text.Text;
 import static javafx.geometry.HPos.RIGHT;
 
 public class PlayerGridPane extends GridPane {
+
+    private Label charNameValueLabel;
+    private  Label charClassValueLabel;
+    private  Label charLevelValueLabel;
+    private Label hpValueLabel;
+    private Label strValueLabel;
+    private Label dexValueLabel;
+    private Label conValueLabel;
+    private Label intValueLabel;
+    private Label wisValueLabel;
+    private Label chaValueLabel;
+    private Label initiativeValueLabel;
+    private TextField damageTextField;
+    private  TextField statusTextField;
+    private TextField saveThrowTextField;
+
     public PlayerGridPane(RegisterPlayer registerPlayer) {
 
         //Player Info
         //"this" is used for whatever gridpane you are using in this instance
         Label charNameLabel = new Label("Character Name: ");
         this.add(charNameLabel, 0, 1);
-        Label charNameValueLabel = new Label(registerPlayer.getPlayerName());
+        charNameValueLabel = new Label(registerPlayer.getPlayerName());
         this.add(charNameValueLabel, 1, 1);
 
         Label charClassLabel = new Label("Character Class: ");
         this.add(charClassLabel, 0, 2);
-        Label charClassValueLabel = new Label(registerPlayer.getPlayerClass());
+        charClassValueLabel = new Label(registerPlayer.getPlayerClass());
         this.add(charClassValueLabel, 1, 2);
 
         Label charLevelLabel = new Label("Player Level: ");
         this.add(charLevelLabel, 0, 3);
-        Label charLevelValueLabel = new Label(String.valueOf(registerPlayer.getPlayerLevel()));
+        charLevelValueLabel = new Label(String.valueOf(registerPlayer.getPlayerLevel()));
         this.add(charLevelValueLabel, 1, 3);
 
         Label hpLabel = new Label("Character HP: ");
         this.add(hpLabel, 0, 4);
-        Label hpValueLabel = new Label(String.valueOf(registerPlayer.getPlayerHP()));
+        hpValueLabel = new Label(String.valueOf(registerPlayer.getPlayerHP()));
         this.add(hpValueLabel, 1, 4);
 
         Label strLabel = new Label("STR: ");
         this.add(strLabel, 0, 5);
-        Label strValueLabel = new Label(String.valueOf(registerPlayer.getPlayerStr()));
+        strValueLabel = new Label(String.valueOf(registerPlayer.getPlayerStr()));
         this.add(strValueLabel, 1, 5);
 
         Label dexLabel = new Label("DEX: ");
         this.add(dexLabel, 0, 6);
-        Label dexValueLabel = new Label(String.valueOf(registerPlayer.getPlayerDex()));
+        dexValueLabel = new Label(String.valueOf(registerPlayer.getPlayerDex()));
         this.add(dexValueLabel, 1, 6);
 
         Label conLabel = new Label("CON: ");
         this.add(conLabel, 0, 7);
-        Label conValueLabel = new Label(String.valueOf(registerPlayer.getPlayerCon()));
+        conValueLabel = new Label(String.valueOf(registerPlayer.getPlayerCon()));
         this.add(conValueLabel, 1, 7);
 
         Label intLabel = new Label("INT: ");
         this.add(intLabel, 0, 8);
-        Label intValueLabel = new Label(String.valueOf(registerPlayer.getPlayerInt()));
+        intValueLabel = new Label(String.valueOf(registerPlayer.getPlayerInt()));
         this.add(intValueLabel, 1, 8);
 
         Label wisLabel = new Label("WIS: ");
         this.add(wisLabel, 0, 9);
-        Label wisValueLabel = new Label(String.valueOf(registerPlayer.getPlayerWis()));
+        wisValueLabel = new Label(String.valueOf(registerPlayer.getPlayerWis()));
         this.add(wisValueLabel, 1, 9);
 
         Label chaLabel = new Label("CHA: ");
         this.add(chaLabel, 0, 10);
-        Label chaValueLabel = new Label(String.valueOf(registerPlayer.getPlayerCha()));
+        chaValueLabel = new Label(String.valueOf(registerPlayer.getPlayerCha()));
         this.add(chaValueLabel, 1, 10);
 
         Label initiativeLabel = new Label("Intiative: ");
         this.add(initiativeLabel, 0, 11);
-        Label initiativeValueLabel = new Label(String.valueOf(registerPlayer.getPlayerInitiative()));
+        initiativeValueLabel = new Label(String.valueOf(registerPlayer.getPlayerInitiative()));
         this.add(initiativeValueLabel, 1, 11);
 
         //Info Affected/Sent
         Label damageLabel = new Label("Damage: ");
         this.add(damageLabel, 0, 12);
-        TextField damageTextField = new TextField();
+        damageTextField = new TextField();
         this.add(damageTextField, 1, 12);
 
         Label statusLabel = new Label("Status Effect: ");
         this.add(statusLabel, 0, 13);
-        TextField statusTextField = new TextField();
+        statusTextField = new TextField();
         this.add(statusTextField, 1, 13);
 
         Label saveThrowLabel = new Label("Saving Throw: ");
         this.add(saveThrowLabel, 0, 14);
-        TextField saveThrowTextField = new TextField();
+        saveThrowTextField = new TextField();
         this.add(saveThrowTextField, 1, 14);
 
         Button btn = new Button("Submit Change");
@@ -110,6 +127,18 @@ public class PlayerGridPane extends GridPane {
                     .setSavingThrow(Integer.parseInt(saveThrowTextField.getText()))
                     .build();
         });
+    }
+
+    public void update(UpdatePlayerStats updatePlayerStats) {
+        charLevelValueLabel.setText(String.valueOf(updatePlayerStats.getPlayerLevel()));
+        hpValueLabel.setText(String.valueOf(updatePlayerStats.getPlayerHP()));
+        strValueLabel.setText(String.valueOf(updatePlayerStats.getPlayerStr()));
+        dexValueLabel.setText(String.valueOf(updatePlayerStats.getPlayerDex()));
+        conValueLabel.setText(String.valueOf(updatePlayerStats.getPlayerCon()));
+        intValueLabel.setText(String.valueOf(updatePlayerStats.getPlayerInt()));
+        wisValueLabel.setText(String.valueOf(updatePlayerStats.getPlayerWis()));
+        chaValueLabel.setText(String.valueOf(updatePlayerStats.getPlayerCha()));
+        initiativeValueLabel.setText(String.valueOf(updatePlayerStats.getPlayerInitiative()));
     }
     
 }
